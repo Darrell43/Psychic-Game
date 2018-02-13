b3050706a78a504263a982d79d1be1f96c859640
Darrell Abney <abbyab43@Darrells-MacBook-Pro.local>
2018-02-12 21:35:18 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,9 @@
 
 // first we must create the varible of guesses which are the alphabet
 
-var alphabet = ["a","b", "c", "d", ]
+var alphabet = Array ["a","b","c",etc];
+
+
+for(i = 0; 0 < alphabet.length; i++);
+
+    alert(b);
