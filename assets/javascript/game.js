a353e0b5ba583e9ff8c29f45b8fe1509a067358c
Darrell Abney <dabney@ceo-psb-dhcp17.rutgers.edu>
2018-02-14 10:36:51 -0500

    Js Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,25 @@
 
 // first we must create the varible of guesses which are the alphabet
 
+
+var alphabet = Array ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var winsCounter = "";
+var lossesCounter = "";
+var guessesLeftCounter = "";
+var gueesesSoFarCounter = "";
+
+
+document.getElementById("checkGuess").onclick = function() {
+
+    alert("hi!");
+}
+
+
 var alphabet = Array ["a","b","c",etc];
+var winsCounter = "";
+var lossesCounter = "";
+var guessesLeftCounter = "";
+var gueesesSoFarCounter = "";
 
 
 for(i = 0; 0 < alphabet.length; i++);
