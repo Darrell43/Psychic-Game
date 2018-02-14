9802341fcf18740a8703d34c6d58563768c91220
Darrell Abney <dabney@ceo-psb-dhcp17.rutgers.edu>
2018-02-14 17:26:37 -0500

    Variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 // first we must create the varible of guesses which are the alphabet
 
 
-var alphabet = Array ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var winsCounter = "";
 var lossesCounter = "";
 var guessesLeftCounter = "";
@@ -12,7 +12,14 @@ var gueesesSoFarCounter = "";
 
 document.getElementById("checkGuess").onclick = function() {
 
-    alert("hi!");
+
+    // we want to create a variable that creates a randdom number
+    var alphabet = Math.random();
+    
+    alphabet = Math.floor(alphabet);
+
+    if(document)
+
 }
 
 
@@ -26,3 +33,32 @@ var gueesesSoFarCounter = "";
 for(i = 0; 0 < alphabet.length; i++);
 
     alert(b);
+
+
+    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var winsCounter = "";
+var lossesCounter = "";
+var guessesLeftCounter = "";
+var gueesesSoFarCounter = "";
+
+
+document.getElementById("checkGuess").onclick = function() {
+
+
+    // we want to create a variable that creates a randdom number
+    var randomNumber = Math.random();
+
+    randomNumber = randomNumber * 25;
+    
+    randomNumber = Math.floor(randomNumber);
+
+    if(document.getElementById("textType").vaule == randomNumber){
+
+        alert("well done!");
+
+    } else {
+
+        alert("Nope! the number was " + randomNumber);
+    }
+
+}
